f4740fba3166098c42d063e0c04326117e769e59
Manvendra Singh Rathod <110388755+Manvendra108@users.noreply.github.com>
2024-05-13 14:41:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 let randomNumber = parseInt(Math.random() * 100 + 1);
 
 const submit = document.querySelector('#subt');
-
 const userInput = document.querySelector('#guessField');
 const guessSlot = document.querySelector('.guesses');
 const remaining = document.querySelector('.lastResult');
